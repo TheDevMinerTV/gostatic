660a74ef164f86c8d45128df43ec0d3367e4efe9
TheDevMinerTV <tobigames200@gmail.com>
2022-11-18 11:17:56 +0100

    chore: add files

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM golang:1.19.3 AS builder
+WORKDIR /src
+
+COPY ./go.sum ./go.mod /src/
+RUN go mod download
+
+COPY . /src
+RUN CGO_ENABLED=0 go build -o /bin/gostatic -ldflags="-w -s"
+
+
+
+FROM alpine:3.16.3 AS runner
+
+COPY --from=builder /bin/gostatic /bin/gostatic
+
+CMD ["/bin/gostatic", "--files", "/static"]
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+var fFilePath = flag.String("files", "/data/files", "Path to static files")
+
+func main() {
+	flag.Parse()
+
+	if *fFilePath == "" {
+		log.Printf("No files path specified")
+	}
+
+	app := fiber.New(fiber.Config{
+		GETOnly:           true,
+		EnablePrintRoutes: true,
+	})
+
+	app.Use(logger.New())
+
+	app.Static("/", *fFilePath)
+
+	if err := app.Listen(":3000"); err != nil {
+		log.Fatal(err)
+	}
+}
