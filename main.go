52fcfa2bbebf2ee57bd2a5b9032cf68b406e8744
TheDevMinerTV <tobigames200@gmail.com>
2022-11-18 12:47:36 +0100

    fix: caching

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"log"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
@@ -24,7 +25,14 @@ func main() {
 
 	app.Use(logger.New())
 
-	app.Static("/", *fFilePath)
+	app.Static("/", *fFilePath, fiber.Static{
+		Compress:      true,
+		Browse:        false,
+		Download:      false,
+		CacheDuration: 12 * time.Hour,
+		// let the browser cache this item for 12 hours, after that it should refresh
+		MaxAge: 43200,
+	})
 
 	if err := app.Listen(":3000"); err != nil {
 		log.Fatal(err)
